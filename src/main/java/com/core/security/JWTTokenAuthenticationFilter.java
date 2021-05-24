fd1db26e5d857c2ef77180d4945224a1e5789529
De Chun Yuan <yuandechunsh@163.com>
2021-05-24 16:09:11 +0800

    add exempt url

--- src/main/java/com/core/security/JWTTokenAuthenticationFilter.java
+++ src/main/java/com/core/security/JWTTokenAuthenticationFilter.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -28,6 +29,9 @@ public class JWTTokenAuthenticationFilter extends OncePerRequestFilter {
     @Value("${jwt.token.header}")
     private String TOKEN_HEADER;
 
+    @Value("${jwt.token.exempt.url}")
+    private String exemptUrl;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -37,10 +41,17 @@ public class JWTTokenAuthenticationFilter extends OncePerRequestFilter {
         //获取当前url
         String currentURL = request.getRequestURL().toString();
         //判断下面的url请求是不是需要豁免的资源
-        List<String> exemptList = List.of("druid", "/api/user/save", "/api/user/query", "swagger", "webjars", "v2", "favicon.ico", ".ico", ".css", ".js");
+        StringBuffer exemptUrlBuff = new StringBuffer("druid,swagger,webjars,v2,favicon.ico,.ico,.css,.js");
+        //添加其余系统豁免url
+        if (StringUtils.isNotBlank(exemptUrl)) {
+            exemptUrlBuff.append("," + exemptUrl);
+        }
+        List<String> exemptList = new ArrayList();
+        exemptList.addAll(Arrays.asList(StringUtils.delimitedToArray(exemptUrlBuff.toString(), ",")));
         for (String exempt : exemptList) {
             if (currentURL.contains(exempt)) {
                 isExempt = true;
+                break;
             }
         }
         //对于没有得到豁免的url请求需要进行token验证

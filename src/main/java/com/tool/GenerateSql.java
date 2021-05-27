168cd6d827b5dc0c4a6cbb5c76b0a2c666a1cb16
De Chun Yuan <yuandechunsh@163.com>
2021-05-27 18:01:50 +0800

    Update GenerateSql.java

--- src/main/java/com/tool/GenerateSql.java
+++ src/main/java/com/tool/GenerateSql.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
-public class CreateDropSql {
+public class GenerateSql {
     private final static String sqlPath = "sql/";
 
     public static void main(String[] args) {

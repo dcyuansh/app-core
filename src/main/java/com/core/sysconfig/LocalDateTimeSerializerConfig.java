4acdd28330eb01f6054a0e1640b78c8dd6dbfb49
De Chun Yuan <yuandechunsh@163.com>
2021-06-02 17:30:03 +0800

    Create LocalDateTimeSerializerConfig.java

---
+++ src/main/java/com/core/sysconfig/LocalDateTimeSerializerConfig.java
@@ -0,0 +1,31 @@
+package com.core.sysconfig;
+
+import com.core.constants.SystemConstant;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+/**
+ * @author spring.yuan
+ * @version 1.0
+ * @desc 全局格式化LocalDateTime格式
+ */
+@Configuration
+public class LocalDateTimeSerializerConfig {
+
+
+    @Bean
+    public LocalDateTimeSerializer localDateTimeDeserializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(SystemConstant.DATE_TIME_FORMAT));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
+    }
+}

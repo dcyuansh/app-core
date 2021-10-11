9a5f36aa5baae078d03101c2882e5db23cba49e3
De Chun Yuan <yuandechunsh@163.com>
2021-10-11 14:11:53 +0800

    add dynamic data source

---
+++ src/main/java/com/core/dbconfig/aop/DynamicDataSourceAspect.java
@@ -0,0 +1,64 @@
+package com.core.dbconfig.aop;
+
+import com.core.dbconfig.annotation.DataSource;
+import com.core.dbconfig.multipledatasource.DataSourceType;
+import com.core.dbconfig.multipledatasource.DynamicDataSourceContextHolder;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author spring.yuan
+ * @version 1.0
+ */
+@Aspect
+@Component
+public class DynamicDataSourceAspect {
+
+    private Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+
+    /**
+     * 在Mapper层添加注解，实现切换数据源
+     */
+    @Pointcut("execution(public * com..*.repository.*.*(..))")
+    public void dataSourcePointCut() {
+    }
+
+
+    @Before("dataSourcePointCut()")
+    public void before(JoinPoint joinPoint) {
+        Object target = joinPoint.getTarget();
+        String method = joinPoint.getSignature().getName();
+        Class<?>[] clazz = target.getClass().getInterfaces();
+        Class<?>[] parameterTypes = ((MethodSignature) joinPoint.getSignature()).getMethod().getParameterTypes();
+        try {
+            Method m = clazz[0].getMethod(method, parameterTypes);
+            //如果方法上存在切换数据源的注解，则根据注解内容进行数据源切换
+            if (m != null && m.isAnnotationPresent(DataSource.class)) {
+                DataSource data = m.getAnnotation(DataSource.class);
+                DataSourceType dataSourceType = data.value();
+                DynamicDataSourceContextHolder.setDataSourceType(dataSourceType);
+                LOG.debug("current thread " + Thread.currentThread().getName() + " add " + dataSourceType + " to ThreadLocal");
+            } else {
+                LOG.debug("no data source specified, so use the default data source.");
+            }
+        } catch (Exception e) {
+            LOG.error("switch datasource fail,current thread " + Thread.currentThread().getName() + " add data to ThreadLocal error", e);
+        }
+    }
+
+    @After("dataSourcePointCut()")
+    public void after(JoinPoint joinPoint) {
+        DynamicDataSourceContextHolder.clearDataSourceType();
+    }
+
+}
--- src/main/java/com/core/dbconfig/config/DynamicDataSourceConfig.java
+++ src/main/java/com/core/dbconfig/config/DynamicDataSourceConfig.java
@@ -1,7 +1,9 @@
 
-package com.core.druid.config;
+package com.core.dbconfig.config;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.core.dbconfig.multipledatasource.DataSourceType;
+import com.core.dbconfig.multipledatasource.DynamicDataSource;
 import org.apache.ibatis.io.VFS;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -19,13 +21,15 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author spring.yuan
  * @version 1.0
  */
 @Configuration
-public class DruidDBConfig {
+public class DynamicDataSourceConfig {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -34,17 +38,44 @@ public class DruidDBConfig {
     private String mybatisMapperLocations;
 
 
-    @Bean(name = "dataSource") // 声明其为Bean实例
-    @Primary // 在同样的DataSource中，首先使用被标注的DataSource
-    @ConfigurationProperties(prefix = "spring.datasource.druid")
-    public DataSource dataSource() {
+    @Bean(name = "masterDataSource") // 声明其为Bean实例
+    //@Primary // 在同样的DataSource中，首先使用被标注的DataSource
+    @ConfigurationProperties(prefix = "spring.datasource.druid.master")
+    public DataSource masterDataSource() {
         return DruidDataSourceBuilder.create().build();
+    }
+
+
+    @Bean(name = "slaveDataSource") // 声明其为Bean实例
+    @ConfigurationProperties(prefix = "spring.datasource.druid.slave")
+    public DataSource slaveDataSource() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+
+    /**
+     * @return
+     * @desc 动态配置多数据源
+     */
+    @Bean(name = "dynamicDataSource")
+    @Primary
+    public DataSource multipleDataSource(@Qualifier("masterDataSource") DataSource masterDb, @Qualifier("slaveDataSource") DataSource slaveDb) {
+        DynamicDataSource multipleDataSource = new DynamicDataSource();
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        targetDataSources.put(DataSourceType.MASTER, masterDb);
+        targetDataSources.put(DataSourceType.SLAVE, slaveDb);
+        //添加数据源
+        multipleDataSource.setTargetDataSources(targetDataSources);
+        //设置默认数据源
+        multipleDataSource.setDefaultTargetDataSource(masterDb);
+        multipleDataSource.afterPropertiesSet();
+        return multipleDataSource;
     }
 
 
     @Bean(name = "sqlSessionFactory")
     @Primary
-    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("dynamicDataSource") DataSource dataSource) throws Exception {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
         //模块依赖打包的时候报错，找不到setTypeAliasesPackage，所以需要添加下面设置
@@ -58,7 +89,7 @@ public class DruidDBConfig {
 
     @Bean(name = "transactionManager")
     @Primary
-    public DataSourceTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
+    public DataSourceTransactionManager transactionManager(@Qualifier("dynamicDataSource") DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
 

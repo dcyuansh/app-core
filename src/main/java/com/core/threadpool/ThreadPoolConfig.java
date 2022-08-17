dd4f46ef29ca1109c1ce26bda9f40d50bdabb31c
De Chun Yuan <yuandechunsh@163.com>
2022-08-17 17:06:10 +0800

    Update ThreadPoolConfig.java

--- src/main/java/com/core/threadpool/ThreadPoolConfig.java
+++ src/main/java/com/core/threadpool/ThreadPoolConfig.java
@@ -52,6 +52,7 @@ public class ThreadPoolConfig {
         executor.setMaxPoolSize(maxPoolSize);  //最大线程数20：线程池最大的线程数，只有在缓冲队列满了之后才会申请超过核心线程数的线程
         executor.setQueueCapacity(queueCapacity); //缓冲队列200：用来缓冲执行任务的队列
         executor.setKeepAliveSeconds(keepAliveSeconds); //允许线程的空闲时间60秒：当超过了核心线程出之外的线程在空闲时间到达之后会被销毁
+        executor.setWaitForTasksToCompleteOnShutdown(true); // 该方法用来设置 线程池关闭 的时候 等待 所有任务都完成后，再继续 销毁 其他的 Bean，这样这些 异步任务 的 销毁 就会先于 数据库连接池对象 的销毁。
         executor.setThreadNamePrefix("AsyncExecutorThread-");//线程池名的前缀：设置好了之后可以方便我们定位处理任务所在的线程池
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());//线程池对拒绝任务(无线程可用)的处理策略
         executor.initialize();
